bb0f09dc5b56982d3f6dc0a764435cce971ed2cf
Student <student@RU-012.local>
2015-09-21 16:15:45 -0600

    fixed spacing issues

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 var Fooditem = function( name, calories, vegan, glutenFree, citrusFree ){
 
-	this.name = name;
-	this.calories = calories;
-	this.vegan = vegan;
+	this.name       = name;
+	this.calories   = calories;
+	this.vegan      = vegan;
 	this.glutenFree = glutenFree;
 	this.citrusFree = citrusFree;
 	// this.stringify=stringify
@@ -18,16 +18,16 @@ Fooditem.prototype.stringify = function(){
 }
 
 
- var burrito = new Fooditem( 'burrito', 500, false, false, true)
+ var burrito          = new Fooditem( 'burrito', 500, false, false, true)
  var chickenBurrito   = new Fooditem('Chicken Burrito', 600, true, true, true)
- var nachos  = new Fooditem( 'Nachos', 1000, false, true, false)
+ var nachos           = new Fooditem( 'Nachos', 1000, false, true, false)
 
 
 var Drink = function( name, description, price, ingredients){
 
-	this.name = name;
-	this.description= description;
-	this.price = price;
+	this.name        = name;
+	this.description = description;
+	this.price       = price;
 	this.ingredients = ingredients;
 }
 
@@ -43,9 +43,9 @@ console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.
 
 var Plate = function( name, description, price, ingredients){
 
-	this.name = name;
-	this.description= description;
-	this.price = price;
+	this.name        = name;
+	this.description = description;
+	this.price       = price;
 	this.ingredients = ingredients;
 
 }
@@ -64,7 +64,7 @@ var margaritaDrink = new Drink ('bad drink', 'alot will get you drunk', 2, ['wat
 
 var guacamolePlate = new Plate ('guacamole', ' best guacamole ever', 15, ['shell', 'meat', 'guacamole', 'sauce'])
 
-var burritoPlate = new Plate ('burrito', 'pants overload goodness', 18,['chicken', 'beans', 'hot sauce', 'mystery meat'])
+var burritoPlate   = new Plate ('burrito', 'pants overload goodness', 18,['chicken', 'beans', 'hot sauce', 'mystery meat'])
 
 
 
