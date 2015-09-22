2e0eb33eee4ce02eb3f1d1470898de431d785caa
Student <student@RU-012.local>
2015-09-22 09:13:25 -0600

    fixed major issues, I believe all problems are now fixed.

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-
-var Fooditem = function( name, calories, vegan, glutenFree, citrusFree ){
-
+var FoodItem = function  (name, calories, vegan, glutenFree, citrusFree) {
 	this.name       = name;
 	this.calories   = calories;
 	this.vegan      = vegan;
 	this.glutenFree = glutenFree;
 	this.citrusFree = citrusFree;
-	// this.stringify=stringify
-
 }
-// console.log(obj.name + ' ' + obj.calories + ' ' +  'is this vegan? ' +  obj.vegan + ' ' + obj.glutenFree + ' ' + obj.citrusFree)
 
-Fooditem.prototype.stringify = function(){
-
-	console.log((this.name + ' ' + this.calories + ' ' +  'is this vegan? ' +  this.vegan + ' ' + this.glutenFree + ' ' + this.citrusFree))
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Food Item-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=\\
+FoodItem.prototype.stringify = function(){
+	var str = ' name: ' + this.name + ', calories: ' + this.calories + ', vegan: ' + this.vegan + ', Gluten Free: ' + this.glutenFree + ', Citrus Free: ' + this.citrusFree + ' '
+	return str
 
 }
 
-
- var burrito          = new Fooditem( 'burrito', 500, false, false, true)
- var chickenBurrito   = new Fooditem('Chicken Burrito', 600, true, true, true)
- var nachos           = new Fooditem( 'Nachos', 1000, false, true, false)
+var guacamole = new FoodItem('guacamole', 100, true, true, false)
+var liquor = new FoodItem('tequila', 200 , true, true, true)
 
 
-var Drink = function( name, description, price, ingredients){
+var burritos = new FoodItem('Big Burrito',2000,false,false,false)
+var taco     = new FoodItem('taco Tuesday',10000,false,false,false)
+var salad    = new FoodItem('Salad',200,true,true,true)
+console.log(burritos.stringify())
+console.log(taco.stringify())
+console.log(salad.stringify())
 
+//=-=-=-=-=-=-=-=-=-=-=-=-Drink=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\\
+
+var Drink = function(name, description, price, ingredients){
 	this.name        = name;
 	this.description = description;
 	this.price       = price;
@@ -32,79 +34,109 @@ var Drink = function( name, description, price, ingredients){
 }
 
 Drink.prototype.stringify = function(){
-
-console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.ingredients))
-
-
-
+	var str = 'name: ' + this.name + ', description: ' + this.description + ", price: " + this.price + ', ingredients: ' + this.ingredients 
+	return str
 
 }
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=Plate=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\\
 
-var Plate = function( name, description, price, ingredients){
-
+var Plate = function(name, description, price, ingredients){
 	this.name        = name;
 	this.description = description;
 	this.price       = price;
 	this.ingredients = ingredients;
-
 }
-
-
 
 Plate.prototype.stringify = function(){
-
-console.log((this.name + ' ' + this.description + ' ' + this.price + ' ' + this.ingredients))
+	var str = 'name: ' + this.name + ', description: ' + this.description + ", price: " + this.price + ', ingredients: ' + this.ingredients 
+	return str
 
 }
 
+Plate.prototype.isVegan = function(){
+	for (var i = 0; i < this.ingredients.length; i++) {
+		if (this.ingredients[i].vegan === false) {
+			return false;
+		}else{
+			return true;
+		};
+	};
+}
 
+Plate.prototype.isGlutenFree = function(){
+	for (var i = 0; i < this.ingredients.length; i++) {
+		if (this.ingredients[i].glutenFree === false) {
+			return false;
+		}else{
+			return true;
+		};
+	};
+}
 
-var margaritaDrink = new Drink ('bad drink', 'alot will get you drunk', 2, ['water', 'lime'])
+Plate.prototype.isCitrusFree = function(){
+	for (var i = 0; i < this.ingredients.length; i++) {
+		if (this.ingredients[i].citrusFree === false) {
+			return false;
+		}else{
+			return true;
+		};
+	};
+}
+ 
+var burritoPlate = new Plate('burrito','delicious', 10, [burritos.stringify()])
+var guacamolePlate = new Plate('Guacamole','delicious', 10, [guacamole.stringify()])
+var tequilaDrink = new Drink('tequila','Lime Flavor', 10, [liquor.stringify()])
 
-var guacamolePlate = new Plate ('guacamole', ' best guacamole ever', 15, ['shell', 'meat', 'guacamole', 'sauce'])
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Order-=-=-=-=-=-=-=-=-=-=-=-\\
 
-var burritoPlate   = new Plate ('burrito', 'pants overload goodness', 18,['chicken', 'beans', 'hot sauce', 'mystery meat'])
-
-
-
-
-//=-=-=-=--=-=-=-=-=-=-=ORDER-=-=-=-=-=\\
-
-
-var Order = function(plate){
-	this.plate = plate;
-
-
+var Order = function(plates){
+	this.plates = plates;
 }
 
 Order.prototype.stringify = function(){
-
-
+	var str = "Plates: " + this.plates
+	return str
 }
 
-var myOrder = new Order([guacamolePlate, burritoPlate])
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-menu-=-=-=-=-=-=-=-=-=-=-=-=-=\\
 
-myOrder.stringify()
-
-
-
-//=-=-=-=-=-=-=-=-=-=-menu=-=-=-=-=-=-=-=\\
-
-var Menu = function(plate){
-	this.plate = plate;
-
-
+var Menu = function(plates){
+	this.plates = plates;
 }
+var mexicanMenu = new Menu([burritoPlate.stringify(), guacamolePlate.stringify(),tequilaDrink.stringify()])
 
 Menu.prototype.stringify = function(){
+	var str = "Menu: " 
+	for (var i = 0; i < this.plates.length; i++) {
+		str += this.plates[i]
+	};
+	return str
+}
 
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=Restaurant-=-=-=-=-=-=-=-=-=-=-\\
+
+var Restaurant = function(name, description, menu){
+	this.name = name;
+	this.description = description;
+	this.menu = menu;
+}
+
+Restaurant.prototype.stringify = function(){
+	var str = 'name: ' + this.name + ', description: ' + this.description + ', Menu: ' + this.menu 
+	return str
 
 }
 
-var myMenu = new Menu([guacamolePlate, burritoPlate, margaritaDrink])
+var mexicanRestaurant = new Restaurant("Mexican land ", 'mexican food', mexicanMenu.stringify())
+var Customer = function(restrictions){
+	this.restrictions = restrictions;
+}
+Customer.prototype.stringify = function(){
+	var str = "Customer restrictions: " + this.restrictions
+	return str
+}
 
-myMenu.stringify()
 
+console.log(mexicanRestaurant.stringify())
 
-//=-=-=-=-=-=-=-=-=-=-Restaurant-==-=-=-=-//
